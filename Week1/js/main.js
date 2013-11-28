b7b72f299ac221775ac6a09947e01a3a42f64a38
Boyd Tiffin <mokono@fullsail.edu>
2013-11-27 19:13:12 -0600

    Week 1 work...

--- Week1/js/main.js
+++ Week1/js/main.js
@@ -1,2 +1,22 @@
 //Boyd Tiffin
 //1312 - ASDI
+
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+		
+$('#addItem').on('pageinit', function(){
+
+		var myForm = $('#formId');
+		    myForm.validate({
+			invalidHandler: function(form, validator) {
+			},
+			submitHandler: function() {
+		var data = myForm.serializeArray();
+			storeData(data);
+		}
+	});
+	
+	//any other code needed for addItem page goes here
+
+});
