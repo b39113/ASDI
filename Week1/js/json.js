6836f47ec90762ceaaeaea0bd72d5d62c1e3a565
Boyd Tiffin <mokono@fullsail.edu>
2013-12-06 15:55:50 -0600

    Ajax work continues...

--- Week1/js/json.js
+++ Week1/js/json.js
@@ -1,47 +1,45 @@
-var json = {
 	"idea1": {
-		"scTitle" : ["JSONTitle:", "Shower Replacement"],
-		"scDate" : ["Service Date:", "2013-11-11"],
-		"scCompany" : ["Servicer Name:", "The Sparks Solution"],
-		"scPhone" : ["Servicer Phone:", "1-515-123-1234"],
-		"scWarranty" : ["Servicer Warranty:", "No"],
-		"scWarDate" : ["Warranty Expires:", ""],
-		"scDesc" : ["Description:", "Replaced shower in the master bathroom due to leak in the floor pan"]
+		"JSONTitle:" : "Shower Replacement",
+		"Service Date:" : "2013-11-11",
+		"Servicer Name:" : "The Sparks Solution",
+		"Servicer Phone:" : "1-515-123-1234",
+		"Servicer Warranty:" : false,
+		"Warranty Expires:" : null,
+		"Description:" : "Replaced shower in the master bathroom due to leak in the floor pan"
 	},
 	"idea2": {
-		"scTitle" : ["JSONTitle:", "Air Exchanger"],
-		"scDate" : ["Service Date:", "2014-08-21"],
-		"scCompany" : ["Servicer Name:", "The Filter People"],
-		"scPhone" : ["Servicer Phone:", "1-515-234-2345"],
-		"scWarranty" : ["Servicer Warranty:", "No"],
-		"scWarDate" : ["Warranty Expires:", ""],
-		"scDesc" : ["Description:", "Replaced filter in air exchanger"]
+		"JSONTitle:" : "Air Exchanger",
+		"Service Date:" : "2014-08-21",
+		"Servicer Name:" : "The Filter People",
+		"Servicer Phone:" : "1-515-234-2345",
+		"Servicer Warranty:" : false,
+		"Warranty Expires:" : null,
+		"Description:" : "Replaced filter in air exchanger"
 	},
 	"idea3": {
-		"scTitle" : ["JSONTitle:", "AC A Coil"],
-		"scDate" : ["Service Date:", "2013-07-11"],
-		"scCompany" : ["Servicer Name:", "HVAC Pros"],
-		"scPhone" : ["Servicer Phone:", "1-515-345-3456"],
-		"scWarranty" : ["Servicer Warranty:", "Yes"],
-		"scWarDate" : ["Warranty Expires:", "2013-11-11"],
-		"scDesc" : ["Description:", "A coil needed to be replaced in the AC unit"]
+		"JSONTitle:" : "AC A Coil",
+		"Service Date:" : "2013-07-11",
+		"Servicer Name:" : "HVAC Pros",
+		"Servicer Phone:" : "1-515-345-3456",
+		"Servicer Warranty:" : "Yes",
+		"Warranty Expires:" : "2013-11-11",
+		"Description:" : "A coil needed to be replaced in the AC unit"
 	},
 	"idea4": {
-		"scTitle" : ["JSONTitle:", "Deck Floor Replacement"],
-		"scDate" : ["Service Date:", "2013-06-09"],
-		"scCompany" : ["Servicer Name:", "The Deck Doctors"],
-		"scPhone" : ["Servicer Phone:", "1-515-456-4567"],
-		"scWarranty" : ["Servicer Warranty:", "Yes"],
-		"scWarDate" : ["Warranty Expires:", "2013-11-11"],
-		"scDesc" : ["Description:", "Replaced floor boards on deck"]
+		"JSONTitle:" : "Deck Floor Replacement",
+		"Service Date:" : "2013-06-09",
+		"Servicer Name:" : "The Deck Doctors",
+		"Servicer Phone:" : "1-515-456-4567",
+		"Servicer Warranty:" : "Yes",
+		"Warranty Expires:" : "2013-11-11",
+		"Description:" : "Replaced floor boards on deck"
 	},
 	"idea5": {
-		"scTitle" : ["JSONTitle:", "Electrical Master"],
-		"scDate" : ["Service Date:", "2012-12-24"],
-		"scCompany" : ["Servicer Name:", "Matt Humphrey"],
-		"scPhone" : ["Servicer Phone:", "1-515-567-5678"],
-		"scWarranty" : ["Servicer Warranty:", "No"],
-		"scWarDate" : ["Warranty Expires:", ""],
-		"scDesc" : ["Description:", "Added new outlet in master bathroom"]
+		"JSONTitle:" : "Electrical Master",
+		"Service Date:" : "2012-12-24",
+		"Servicer Name:" : "Matt Humphrey",
+		"Servicer Phone:" : "1-515-567-5678",
+		"Servicer Warranty:" : false,
+		"Warranty Expires:" : null,
+		"Description:" : "Added new outlet in master bathroom"
 	}
-}
